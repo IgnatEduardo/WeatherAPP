838431b8278ea85fedc22609b39c11a239afec35
SoareCristi <cristi2001s@gmail.com>
2023-05-16 12:03:00 +0300

    Revert "Added daily forecast"
    This reverts commit 0f5f1d7bdb116c080de5a9d14de7696982066777.

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -6,12 +6,10 @@ import UIKit
 import CoreLocation
 import UserNotifications
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate{
+class ViewController: UIViewController, CLLocationManagerDelegate{
 
     @IBOutlet var table: UITableView!
     
-    var dailyModels = [DailyForecast]()
-    var hourlyModels = [HourlyForecast]()
     let locationManager = CLLocationManager()
     var currentLocation: CLLocation?
     
@@ -19,12 +17,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        table.register(HourlyTableViewCell.nib(), forCellReuseIdentifier: HourlyTableViewCell.identifier)
-        table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
-        
-        table.delegate = self
-        table.dataSource = self
         
         view.backgroundColor = UIColor(red: 227/255.0, green: 244/255.0, blue: 254/255.0, alpha: 1.0)
         
@@ -137,38 +129,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     }
                     
                     print(dailyForecasts[0])
-                    
-                    self.dailyModels.append(contentsOf: dailyForecasts)
-
-                    self.hourlyModels = hourlyForecasts
-                    // Update UI
-                    DispatchQueue.main.async {
-                        // do something with hourlyForecasts and dailyForecasts
-                        DispatchQueue.main.async {
-                            self.table.reloadData()
-                        }
-                    }
                 }.resume()
             }.resume()
         }.resume()
-    }
-    
-    //Table
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dailyModels.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //return UITableViewCell()
-        let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as! WeatherTableViewCell
-        cell.configure(with: dailyModels[indexPath.row])
-        cell.backgroundColor = UIColor(red: 227/255.0, green: 244/255.0, blue: 254/255.0, alpha: 0.5)
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
     }
 
 }
